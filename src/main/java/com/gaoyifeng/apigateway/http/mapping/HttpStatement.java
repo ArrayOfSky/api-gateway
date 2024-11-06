ed202afd46395c822c0588d4c45e24fc7c812ba6
Gaoyifeng <104308413+ArrayOfSky@users.noreply.github.com>
2024-11-06 21:48:08 +0800

    http模块化

---
+++ src/main/java/com/gaoyifeng/apigateway/http/mapping/HttpStatement.java
@@ -0,0 +1,53 @@
+package com.gaoyifeng.apigateway.http.mapping;
+
+import com.gaoyifeng.apigateway.http.HttpCommandType;
+
+/**
+ * @author gaoyifeng
+ * @Classname HttpStatement
+ * @Description TODO 网关接口映射信息
+ * @Date 2024/11/5 19:52
+ * @Created by gaoyifeng
+ */
+public class HttpStatement {
+
+    /** 应用名称； */
+    private String application;
+    /** 服务接口；RPC、其他 */
+    private String interfaceName;
+    /** 服务方法；RPC#method */
+    private String methodName;
+    /** 网关接口 */
+    private String uri;
+    /** 接口类型；GET、POST、PUT、DELETE */
+    private HttpCommandType httpCommandType;
+
+    public HttpStatement(String application, String interfaceName, String methodName, String uri, HttpCommandType httpCommandType) {
+        this.application = application;
+        this.interfaceName = interfaceName;
+        this.methodName = methodName;
+        this.uri = uri;
+        this.httpCommandType = httpCommandType;
+    }
+
+    public String getApplication() {
+        return application;
+    }
+
+    public String getInterfaceName() {
+        return interfaceName;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public HttpCommandType getHttpCommandType() {
+        return httpCommandType;
+    }
+
+}
